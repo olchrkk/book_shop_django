74877a24d26ce7179865c9f262cb549d8f435f0f
olchrkk <98900779+olchrkk@users.noreply.github.com>
2022-12-05 12:40:44 +0300

    - add homework-22

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.urls import path, include
+from .views import IndexView
 
 
 urlpatterns = [
-
+    path('registration/', IndexView.as_view(), name='registration')
 ]
